4542911179f864aac9b09f99a440da9256ff51bd
Rouge-ops <114273102+Rouge-ops@users.noreply.github.com>
2026-08-24 19:19:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
+console.log("Javascript is working!");
 const hamburger=document.getElementById("hamburger");
 const navLinks=document.getElementById("navLinks");
-
-hamburger.addEventListener("click", () => {navlinks.classlist.toggle("active");});
+hamburger.addEventListener("click", () => {if(navLinks.classList.contains("active")){navLinks.classList.remove("active");}else{navLinks.classList.add("active");}});
