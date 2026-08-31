8d20195be5e71b0f158c4b755a6dee71d5ff56d5
Rouge-ops <114273102+Rouge-ops@users.noreply.github.com>
2026-08-31 10:51:14 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+
 console.log("Javascript is working!");
 const hamburger=document.getElementById("hamburger");
 const navLinks=document.getElementById("navLinks");
-hamburger.addEventListener("click", () => {if(navLinks.classList.contains("active")){navLinks.classList.remove("active");}else{navLinks.classList.add("active");}});
+if(hamburger && navLinks) {
+hamburger.addEventListener("click", () => {if(navLinks.classList.contains("active"))
+    {navLinks.classList.remove("active");}
+    else{navLinks.classList.add("active");}});}
+
+const homeButton = document.querySelector(".home-button");
+homeButton.classList.add("hidden");
+let lastScrollY = window.scrollY;
+let scrollUpDistance = 0;
+window.addEventListener("scroll", () => {const currentScrollY = window.scrollY; 
+    if (currentScrollY > lastScrollY) { scrollUpDistance = 0;
+    homeButton.classList.add("hidden");
+} else if (currentScrollY < lastScrollY) {scrollUpDistance += lastScrollY -currentScrollY;
+    if (scrollUpDistance >= 30) {
+        homeButton.classList.remove("hidden");}} 
+        lastScrollY = currentScrollY;});
